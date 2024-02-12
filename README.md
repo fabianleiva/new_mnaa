b2b90a0e29c7fd7bb4f9730847970634bb04b590
Fabian Leiva <fabianleivacortes@gmail.com>
2024-02-12 13:01:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Max Núñez Arquitectos
+
+React App 
+
+Website developed by Fabián Leiva
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Max Núñez Arquitectos</h1>
+    </>
+  )
+}
+
+export default App
