729a99a3a030c2af85c587125d0261fd27549819
Fabian Leiva <fabianleivacortes@gmail.com>
2024-03-13 11:40:24 -0300

    navbar edit setActiveClass

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,9 +3,7 @@ import { NavLink, useLocation } from "react-router-dom";
 const NavigationBar = () => {
   const { pathname } = useLocation();
 
-  const setActiveClass = (path) => {
-    return pathname.startsWith(path) ? "active" : "inactive";
-  };
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : "inactive");
 
   // Determina si la segunda línea de la barra de navegación debe mostrarse o no en proyectos
   const showProjectsSecondLine =
@@ -19,30 +17,30 @@ const NavigationBar = () => {
         <nav className="flex justify-between fixed top-6 inset-x-6 z-[100] mnaaBold">
           <div>
             <div className="flex text-lg md:text-xl webTitle">
-              <NavLink className={setActiveClass("/")} to="/">
+              <NavLink className={setActiveClass} to="/">
                 <h3 className="pr-3">Proyectos</h3>
               </NavLink>
               <NavLink
-                className={setActiveClass("/archive")}
+                className={setActiveClass}
                 to="/archive/publications"
               >
                 <h3 className="pr-3">Archivo</h3>
               </NavLink>
-              <NavLink className={setActiveClass("/office")} to="/office">
+              <NavLink className={setActiveClass} to="/office">
                 <h3 className="pr-3">Oficina</h3>
               </NavLink>
-              <NavLink className={setActiveClass("/contact")} to="/contact">
+              <NavLink className={setActiveClass} to="/contact">
                 <h3 className="pr-3">Contacto</h3>
               </NavLink>
             </div>
             {showProjectsSecondLine && (
               <div className="flex text-lg md:text-xl webTitle">
-                <NavLink className={setActiveClass("/")} to="/">
+                <NavLink className={setActiveClass} to="/">
                   <h3 className="pr-1">Destacados</h3>
                 </NavLink>
                 <h3 className="pr-1 text-[#242424]">/</h3>
                 <NavLink
-                  className={setActiveClass("/projects/all")}
+                  className={setActiveClass}
                   to="/projects/all"
                 >
                   <h3 className="pr-1">Todos</h3>
@@ -52,14 +50,14 @@ const NavigationBar = () => {
             {showArchiveSecondLine && (
               <div className="flex text-lg md:text-xl webTitle">
                 <NavLink
-                  className={setActiveClass("/archive/publications")}
+                  className={setActiveClass}
                   to="/archive/publications"
                 >
                   <h3 className="pr-1">Publicaciones</h3>
                 </NavLink>
                 <h3 className="pr-1 text-[#242424]">/</h3>
                 <NavLink
-                  className={setActiveClass("/archive/conferences")}
+                  className={setActiveClass}
                   to="/archive/conferences"
                 >
                   <h3 className="pr-1">Conferencias</h3>
