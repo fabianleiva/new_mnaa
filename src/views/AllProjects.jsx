ac58c46c673ecbfc3428bf5881da8796b3d58842
Fabian Leiva <fabianleivacortes@gmail.com>
2024-08-08 13:05:31 -0400

    edits list items

--- src/views/AllProjects.jsx
+++ src/views/AllProjects.jsx
@@ -1,19 +1,18 @@
 import AllProjectsList from "../components/AllProjectsList.jsx";
 
 const AllProjects = () => {
-
   return (
     <>
       <main className="mt-[25vh]">
-        <div className="fixed top-0 w-full bg-[#fafafa] h-[25vh] text-red-500"></div>
-        <div className="-translate-y-full fixed grid grid-cols-8 w-[50vw] mx-6 font-[EB-Garamond-Italic] text-sm text-[#bebebe]">
-          <p className="text-left col-span-3">Proyecto</p>
-          <p className="text-left col-span-2">Lugar</p>
-          <p className="text-left">Superficie</p>
-          <p className="text-left">Categoria</p>
-          <p className="text-right">Year</p>
+        <div className="fixed top-0 w-full bg-[#fafafa] h-[25vh] text-red-500 z-50"></div>
+        <div className="fixed grid grid-cols-9 px-6 font-[supreme-bold] text-[#262523] uppercase w-full bg-[#fafafa] text-xs lg:text-sm z-50">
+          <p className="text-left col-span-8 md:col-span-3">Proyecto</p>
+          <p className="text-left col-span-3 hidden md:block">Lugar</p>
+          <p className="text-left hidden md:block">Superficie</p>
+          <p className="text-left hidden md:block">Tipologia</p>
+          <p className="text-right">Año</p>
+          <hr className="col-span-9 h-[1px] bg-[#262523] border-0 rounded dark:bg-[#262523] translate-y-[-1px]" />
         </div>
-        <hr className="fixed w-[50vw] ml-6 h-[1px] bg-[#bebebe] border-0 rounded dark:bg-[#bebebe] z-50	z-index: 50 ;" />
         <AllProjectsList />
       </main>
     </>
