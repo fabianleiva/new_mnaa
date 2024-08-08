1581f5144f80cad54dfc90f2a9bb5aa45ef7872b
Fabian Leiva <fabianleivacortes@gmail.com>
2024-08-08 13:07:01 -0400

    edit content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
   return (
     <>
       <footer className="mt-24 mb-6">
-        <div className="flex justify-between font-[bookAntiquaItalic] text-[#bebebe]">
+        <div className="flex justify-between font-[supreme-thin] text-[#a0a0a0] text-xs">
           <p></p>
-          <p>Dev. by Fabian Leiva</p>
+          <p>dev. by Fabian Leiva</p>
         </div>
       </footer>
     </>
