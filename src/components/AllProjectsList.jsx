88c963effa1cb8d145c7a2ce2b1ac8d54d56f7bb
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2025-05-21 16:14:07 -0400

    set usenavigate, and add validations to show or not show some info

--- src/components/AllProjectsList.jsx
+++ src/components/AllProjectsList.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { ApiContext } from "../context/ApiContext";
+import { useNavigate } from "react-router-dom";
 
 const AllProjectsList = () => {
   const { projects } = useContext(ApiContext);
@@ -7,9 +8,10 @@ const AllProjectsList = () => {
   const [openProjects, setOpenProjects] = useState([]);
   const [animatingProjects, setAnimatingProjects] = useState({});
 
+  const navigate = useNavigate();
+
   const handleProjectClick = (projectId) => {
     if (openProjects.includes(projectId)) {
-      // Si el proyecto ya está abierto, inicia la animación de cierre
       setAnimatingProjects((prev) => ({
         ...prev,
         [projectId]: "collapse-enter",
@@ -21,9 +23,8 @@ const AllProjectsList = () => {
           const { [projectId]: _, ...rest } = prev;
           return rest;
         });
-      }, 1000); // Duración de la animación de cierre
+      }, 1000);
     } else {
-      // Si el proyecto no está abierto, simplemente ábrelo
       setOpenProjects([...openProjects, projectId]);
       setAnimatingProjects((prev) => ({
         ...prev,
@@ -32,57 +33,115 @@ const AllProjectsList = () => {
     }
   };
 
+  const isValidField = (value) =>
+    value && value.trim() !== "" && value.trim() !== "-";
+
+  const visibleProjects = projects.filter((p) =>
+    isValidField(p.acf.project_title)
+  );
+
   return (
-    <section className="mx-6 mt-12">
+    <section className="mx-3 mt-12">
       <div className="mb-32">
-        <div className="h-4 lg:h-6"></div>
-        {projects.map((p) => (
+        <div className="h-6"></div>
+        {visibleProjects.map((p) => (
           <div
             onMouseEnter={() => setHoveredProjectId(p.id)}
+            onMouseLeave={() => setHoveredProjectId(null)}
             onClick={() => handleProjectClick(p.id)}
             key={p.id}
-            className={`${
-              hoveredProjectId === p.id
-                ? "grid grid-cols-9 justify-between text-xs lg:text-sm font-[supreme-bold] text-[#262523] uppercase tracking-wider"
-                : "grid grid-cols-9 justify-between text-xs lg:text-sm font-[supreme-light] text-[#999999] uppercase tracking-wider"
+            className={`grid grid-cols-3 cursor-pointer transition-colors duration-300 ${
+              hoveredProjectId === p.id || openProjects.includes(p.id)
+                ? "font-[supreme-book] text-[#242424]"
+                : "font-[supreme-book] text-[#adadad]"
             }`}
           >
-            <p className="text-left col-span-8 md:col-span-3 py-1">
+            <p className="col-span-2 lg:col-span-1 text-left py-1">
               {p.acf.project_title}
             </p>
-            <p className="text-left hidden md:block col-span-3 py-1">
-              {p.acf.location}
-            </p>
-            <p className="text-left hidden md:block py-1">{p.acf.surface}</p>
-            <p className="text-left hidden md:block py-1">
-              {p.acf.category.name}
-            </p>
-            <p className="text-right py-1">{p.acf.year}</p>
+            <p className="text-left hidden lg:block py-1">{p.acf.location}</p>
+
+            <div className="lg:grid lg:grid-cols-3 flex justify-end">
+              <p className="text-left hidden lg:block py-1">{p.acf.surface}</p>
+              <p className="text-left hidden lg:block py-1">
+                {p.acf.category.name}
+              </p>
+              <p className="text-right py-1">{p.acf.year}</p>
+            </div>
+
             <div
-              className={`col-span-9 flex justify-end overflow-hidden ${
+              className={`col-span-3 flex justify-end overflow-hidden ${
                 animatingProjects[p.id] || ""
               }`}
             >
               {openProjects.includes(p.id) && (
-                <div className="flex flex-col md:flex-row justify-between w-full font-[supreme-light]">
-                  <div className="flex flex-col text-left py-6 h-[120rem] min-w-[30vw] md:max-w-[30vw] mr-6">
-                    <p>Arquitectos: {p.acf.architects}</p>
-                    <p>Colaboradores: {p.acf.colaborators}</p>
-                    <p>Ubicación: {p.acf.location}</p>
-                    <p>Año: {p.acf.year}</p>
-                    <p>Superficie: {p.acf.surface}</p>
+                <div className="flex flex-col lg:flex-row justify-between w-full font-[supreme-book]">
+                  <div className="flex flex-col text-left py-6 h-full min-w-[30vw] lg:max-w-[30vw] mr-6 justify-between">
+                    <div>
+                      {isValidField(p.acf.architects) && (
+                        <p>Arquitectura: {p.acf.architects}</p>
+                      )}
+                      {isValidField(p.acf.lead_architects) && (
+                        <p>Arquitectos a cargo: {p.acf.lead_architects}</p>
+                      )}
+                      {isValidField(p.acf.associate_architect) && (
+                        <p>
+                          Arquitectos asociados: {p.acf.associate_architect}
+                        </p>
+                      )}
+                      {isValidField(p.acf.team) && <p>Equipo: {p.acf.team}</p>}
+                      {isValidField(p.acf.location) && (
+                        <p>Ubicación: {p.acf.location}</p>
+                      )}
+                      {isValidField(p.acf.year) && <p>Año: {p.acf.year}</p>}
+                      {isValidField(p.acf.surface) && (
+                        <p>Superficie: {p.acf.surface}</p>
+                      )}
+                      {isValidField(p.acf.client) && (
+                        <p>Cliente: {p.acf.client}</p>
+                      )}
+                    </div>
+
+                    <div className="hidden lg:block">
+                      {p.acf.project_filter === "destacado" && (
+                        <button
+                          className="text-[#adadad] hover:text-[#242424] text-left"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            navigate(`/projects/selected/${p.slug}`);
+                          }}
+                        >
+                          {">> Ir al proyecto"}
+                        </button>
+                      )}
+                    </div>
                   </div>
-                  <div className="flex justify-end">
+
+                  <div className="flex justify-between items-end">
+                    <div className="lg:hidden">
+                      {p.acf.project_filter === "destacado" && (
+                        <button
+                          className="text-[#adadad] hover:text-[#242424] py-6 text-left"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            navigate(`/projects/selected/${p.slug}`);
+                          }}
+                        >
+                          {">> Ir al proyecto"}
+                        </button>
+                      )}
+                    </div>
                     <img
                       src={p.acf.cover}
                       alt={p.acf.project_title}
-                      className="py-6 max-w-fit max-h-[30vh] md:max-h-[50vh]"
+                      className="py-6 max-w-[50vw] max-h-[30vh] lg:max-h-[50vh]"
                     />
                   </div>
                 </div>
               )}
             </div>
-            <hr className="col-span-9 h-[1px] bg-[#262523] border-0 rounded dark:bg-[#262523]" />
+
+            <hr className="col-span-3 h-[1px] bg-[#adadad] border-0 rounded" />
           </div>
         ))}
       </div>
