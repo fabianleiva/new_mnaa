dfd446b9c0c2daab68267d9ce2b5185a33913134
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2025-05-21 16:15:20 -0400

    remove recognitions, change style with tailwind in conferences

--- src/components/Conferences.jsx
+++ src/components/Conferences.jsx
@@ -1,128 +1,121 @@
-
 const Conferences = () => {
   return (
-    <main className="mt-[25vh] px-6">
-      <article>
-        {/* Recognitions */}
-        <section className="my-48">
-          <div className="text-base uppercase font-[supreme-light]">
-            {/* Conferences */}
-            <div className="text-left mt-24 col-span-2">
-              <p className="col-span-2 uppercase font-['supreme-bold'] text-xl">
-                Conferencias
-              </p>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>Chile Day / Emerging Chilean Aarchitecture / AIA NY, EEUU</p>
-                <p className="ml-6">2023</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  Viaje Topografico / Colegio de Arquitectos de Mendoza /
-                  Argentina
-                </p>
-                <p className="ml-6">2023</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  1er Encuentro Internacional de Arquitectura / Montevideo,
-                  Uruguay
-                </p>
-                <p className="ml-6">2023</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  Topographical Journey / Neutra VDL / Los Angeles, USA
-                  (Workshop)
-                </p>
-                <p className="ml-6">2023</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>Topographical Journey / Harvard GSD / Boston, EEUU</p>
-                <p className="ml-6">2023</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>Viaje Topografico / UC / Santiago, Chile</p>
-                <p className="ml-6">2023</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>Topographical Journey / House of Oris / Zagreb, Croacia</p>
-                <p className="ml-6">2022</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>WAVE / IUAV / Venecia, Italia (Workshop)</p>
-                <p className="ml-6">2019</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  How Do We Live? / Universidad de Liverpool / Londres,
-                  Inglaterra
-                </p>
-                <p className="ml-6">2018</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  Constellation of Thoughts, Leibniz Universitat / Hannover,
-                  Alemania
-                </p>
-                <p className="ml-6">2017</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  Constellation of Thoughts, Technische Universitat / Dortmund,
-                  Alemania
-                </p>
-                <p className="ml-6">2017</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>Constructing Practice, Columbia University / NY, EEUU</p>
-                <p className="ml-6">2017</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  Max Nunez Arquitectos: Concursos Públicos / UC / Santiago,
-                  Chile
-                </p>
-                <p className="ml-6">2015</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  Max Nunez Arquitectos: Obra Reciente / Universidad de BA /
-                  Argentina
-                </p>
-                <p className="ml-6">2014</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>Topographical Variations / University of Arizona / EEUU</p>
-                <p className="ml-6">2013</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-              <div className="flex justify-between mt-2 align-between">
-                <p>
-                  DRN Arquitectos / Bienal de Arquitectura Latinoamericana /
-                  Pamplona, Espana
-                </p>
-                <p className="ml-6">2013</p>
-              </div>
-              <hr className="col-span-6 w-full h-[1px] bg-[#262523] border-0 rounded dark:bg-gray-700 mt-2" />
-            </div>
+    <main className="mt-[20vh] ">
+      <div className="fixed top-0 w-full bg-[#fafafa] h-[20vh] z-50"></div>
+      <div className="fixed left-0 px-3 grid grid-cols-[2fr_1fr_2fr] text-[#242424] uppercase w-full bg-[#fafafa] z-50">
+        <p className="text-left">Conferencias</p>
+        <p></p>
+        <p className="text-right">Año</p>
+
+        <hr className="col-span-3 h-[1px] bg-[#adadad] border-0 rounded dark:bg-[#242424] translate-y-[-1px]" />
+      </div>
+      <div className="mt-[20vh]  ">
+        {/* Conferences */}
+        <div className="text-left pt-6 col-span-2 mb-40 text-[#adadad] font-[supreme-book]">
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>Chile Day / Emerging Chilean Aarchitecture / AIA NY, EEUU</p>
+            <p className="ml-6">2023</p>
           </div>
-        </section>
-      </article>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              Viaje Topografico / Colegio de Arquitectos de Mendoza / Argentina
+            </p>
+            <p className="ml-6">2023</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              1er Encuentro Internacional de Arquitectura / Montevideo, Uruguay
+            </p>
+            <p className="ml-6">2023</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              Topographical Journey / Neutra VDL / Los Angeles, USA (Workshop)
+            </p>
+            <p className="ml-6">2023</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>Topographical Journey / Harvard GSD / Boston, EEUU</p>
+            <p className="ml-6">2023</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>Viaje Topografico / UC / Santiago, Chile</p>
+            <p className="ml-6">2023</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>Topographical Journey / House of Oris / Zagreb, Croacia</p>
+            <p className="ml-6">2022</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>WAVE / IUAV / Venecia, Italia (Workshop)</p>
+            <p className="ml-6">2019</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              How Do We Live? / Universidad de Liverpool / Londres, Inglaterra
+            </p>
+            <p className="ml-6">2018</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              Constellation of Thoughts, Leibniz Universitat / Hannover,
+              Alemania
+            </p>
+            <p className="ml-6">2017</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              Constellation of Thoughts, Technische Universitat / Dortmund,
+              Alemania
+            </p>
+            <p className="ml-6">2017</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>Constructing Practice, Columbia University / NY, EEUU</p>
+            <p className="ml-6">2017</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              Max Nunez Arquitectos: Concursos Públicos / UC / Santiago, Chile
+            </p>
+            <p className="ml-6">2015</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              Max Nunez Arquitectos: Obra Reciente / Universidad de BA /
+              Argentina
+            </p>
+            <p className="ml-6">2014</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>Topographical Variations / University of Arizona / EEUU</p>
+            <p className="ml-6">2013</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+          <div className="flex justify-between mt-1 align-between hover:text-[#242424]">
+            <p>
+              DRN Arquitectos / Bienal de Arquitectura Latinoamericana /
+              Pamplona, Espana
+            </p>
+            <p className="ml-6">2013</p>
+          </div>
+          <hr className="col-span-6 w-full h-[1px] bg-[#adadad] border-0 rounded  mt-1" />
+        </div>
+      </div>
     </main>
   );
 };
