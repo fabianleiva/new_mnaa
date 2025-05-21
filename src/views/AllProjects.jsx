00665f1c262186c17b377deea8509f4aa7f5233a
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2025-05-21 16:23:33 -0400

    adjuts style with tailwind

--- src/views/AllProjects.jsx
+++ src/views/AllProjects.jsx
@@ -3,15 +3,19 @@ import AllProjectsList from "../components/AllProjectsList.jsx";
 const AllProjects = () => {
   return (
     <>
-      <main className="mt-[25vh]">
-        <div className="fixed top-0 w-full bg-[#fafafa] h-[25vh] text-red-500 z-50"></div>
-        <div className="fixed grid grid-cols-9 px-6 font-[supreme-bold] text-[#262523] uppercase w-full bg-[#fafafa] text-xs lg:text-sm z-50">
-          <p className="text-left col-span-8 md:col-span-3">Proyecto</p>
-          <p className="text-left col-span-3 hidden md:block">Lugar</p>
-          <p className="text-left hidden md:block">Superficie</p>
-          <p className="text-left hidden md:block">Tipologia</p>
-          <p className="text-right">Año</p>
-          <hr className="col-span-9 h-[1px] bg-[#262523] border-0 rounded dark:bg-[#262523] translate-y-[-1px]" />
+      <main className="mt-[20vh] ">
+        <div className="fixed top-0 w-full bg-[#fafafa] h-[20vh] z-50"></div>
+        <div className="fixed grid grid-cols-2 lg:grid-cols-3 px-3 text-[#242424] uppercase w-full bg-[#fafafa] text-base z-50">
+          <p className="text-left">Proyecto</p>
+          <p className="text-left hidden lg:block">Lugar</p>
+
+          <div className="flex justify-end lg:grid lg:grid-cols-3">
+            <p className="text-left hidden lg:block">Superficie</p>
+            <p className="text-left hidden lg:block">Tipología</p>
+            <p className="text-right">Año</p>
+          </div>
+
+          <hr className="col-span-3 h-[1px] bg-[#adadad] border-0 rounded dark:bg-[#242424] translate-y-[-1px]" />
         </div>
         <AllProjectsList />
       </main>
