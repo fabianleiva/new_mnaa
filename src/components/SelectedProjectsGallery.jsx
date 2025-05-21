b5ad08a9382cffa51ce41b8a9a85a9a82ab19ab0
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2025-05-21 16:21:31 -0400

    change 78 and 71 proyect in gallery order, little adjust on style with tailwind

--- src/components/SelectedProjectsGallery.jsx
+++ src/components/SelectedProjectsGallery.jsx
@@ -18,27 +18,34 @@ const FeaturedGallery = () => {
     const index = projects.findIndex((e) => e.acf.project_id === projectId);
     if (index !== -1) {
       const project = projects[index];
-      const imageClass = projectId === "69" ? "min-w-full ml-[-2.5rem] mb-6" : "";
+      const imageClass =
+        projectId === "69" ? "min-w-full ml-[-2.5rem] mb-6" : "";
       const isHovered = hoveredImage === projectId;
 
       return (
         <div
           key={project.id}
-          className="min-w-full flex justify-center h-fit relative text-xl mnaaBold"
+          className="flex justify-center h-fit relative"
           onMouseEnter={() => handleMouseEnter(projectId)}
           onMouseLeave={handleMouseLeave}
         >
-          <Link className="flex bg-[#fafafa]" to={`/projects/selected/${project.slug}`}>
+          <Link
+            className="flex bg-[#fafafa] text-[#fafafa] hover:text-[#242424]"
+            to={`/projects/selected/${project.slug}`}
+          >
             <div
-              className={`projectName absolute z-[-10]`}
+              className={`transition-all duration-300 ease-in-out absolute z-[-10] top`}
               style={{ top: isHovered ? "-1.7rem " : 0 }}
             >
-              <span className="text-[1.1rem] hidden md:block">{project.acf.project_title}</span>
+              <span className="hidden md:block">
+                {project.acf.project_title}
+              </span>
             </div>
             <img
-              className={`${imageClass} featuredImage min-w-full z-0`}
+              className={`${imageClass} w-full h-auto z-0 `}
               src={project.acf.cover}
               alt={project.slug}
+              loading="lazy"
             />
           </Link>
         </div>
@@ -48,69 +55,69 @@ const FeaturedGallery = () => {
   };
 
   return (
-    <section className="flex flex-col">
-      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_2fr] gap-5 mb-5 md:mb-40">
+    <section className="flex flex-col tracking-wider">
+      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("54")}
-        {renderProjects("71")}
+        {renderProjects("78")}
         {renderProjects("72")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[1fr_2fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[1fr_2fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("70")}
         {renderProjects("74")}
         {renderProjects("75")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[2fr_2fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[2fr_2fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("47")}
         {renderProjects("61")}
         {renderProjects("79")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("73")}
-        {renderProjects("78")}
+        {renderProjects("71")}
         {renderProjects("57")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[1fr_2fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[1fr_2fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("69")}
         {renderProjects("67")}
         {renderProjects("62")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[2fr_2fr_1fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[2fr_2fr_1fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("77")}
         {renderProjects("64")}
         {renderProjects("65")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("50")}
         {renderProjects("49")}
         {renderProjects("41")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[2fr_2fr_1fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[2fr_2fr_1fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("60")}
         {renderProjects("53")}
         {renderProjects("51")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[3fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[3fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("39")}
         {renderProjects("45")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[1fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[1fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("13")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[3fr_2fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[3fr_2fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("18")}
         {renderProjects("21")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_1fr_1fr] gap-5 mb-5 md:mb-40">
+      <div className="flex flex-col md:grid grid-cols-[2fr_1fr_1fr_1fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
         {renderProjects("14")}
-        <img className="block" src="" alt="" />
+        <div></div>
         {renderProjects("15")}
         {renderProjects("10")}
       </div>
-      <div className="flex flex-col md:grid grid-cols-[1fr_1fr_2fr_1fr] gap-5 mb-5 md:mb-40">
-        <img className="block" src="" alt="" />
+      <div className="flex flex-col md:grid grid-cols-[1fr_1fr_2fr_1fr] gap-3 mb-3 md:mb-20 2xl:mb-40">
+        <div></div>
         {renderProjects("08")}
         {renderProjects("06")}
-        <img className="block" src="" alt="" />
+        <div></div>
       </div>
     </section>
   );
